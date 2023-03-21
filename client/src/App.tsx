a62039ecd184a2a1f4fb574e19c870a1eff149cc
BRITHE <bthellier51@gmail.com>
2023-03-21 06:46:42 +0100

    added routes for components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,18 +10,24 @@ import Informations from './screens/Informations';
 import Users from './screens/Users';
 import Comments from './screens/Comments';
 import CreatePost from './screens/CreatePost';
+import Likes from './screens/Likes';
+import Posts from './screens/Posts';
+import Post from './screens/Post';
 
 const App = () => {
 	return (
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout children={<Main />} />} />
-				<Route path="/new" element={<CreatePost />} />
+				<Route path="/new" element={<Layout children={<CreatePost />} />} />
 				<Route path="/users" element={<Users />} />
 				<Route path="/:username" element={<Profile />} />
 				<Route path="/:username/followers" element={<Followers />} />
 				<Route path="/:username/following" element={<Following />} />
+				<Route path="/post/:postId" element={<Layout children={<Post />} />} />
 				<Route path="/profile" element={<Informations />} />
+				<Route path="/posts" element={<Posts />} />
+				<Route path="/likes" element={<Likes />} />
 				<Route path="/comments" element={<Comments />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
