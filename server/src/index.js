d5906d414090cd81aad170af0efef6f6ca63cfd3
BRITHE <bthellier51@gmail.com>
2023-03-21 11:37:48 +0100

    .

--- server/src/index.js
+++ server/src/index.js
@@ -25,21 +25,24 @@ app.use('/api', router);
 // app.post('/auth/register', register);
 // app.post('/auth/login', login);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('watzeid - API v1');
+	});
+}
+
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.listen(process.env.PORT, () => {
 	console.log(`server on http://localhost:${process.env.PORT}`);
 });
-
-if (process.env.NODE_ENV === 'production') {
-	//*Set static folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	);
-}
 
 // app.use((err, req, res, next) => {
 // 	if (err.type === 'auth') {
