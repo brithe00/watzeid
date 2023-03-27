9745c7874a37a0f4e8c06d747ee3703eb485f24e
BRITHE <bthellier51@gmail.com>
2023-03-27 09:54:54 +0200

    added front api call for comments for a post

--- client/src/api/comment.js
+++ client/src/api/comment.js
@@ -37,3 +37,32 @@ export const deleteComment = (postId) => {
 		.delete(`/api/posts/${postId}/comment`, config)
 		.then((res) => res.data);
 };
+
+export const getCommentsForPost = (postId) => {
+	const token = localStorage.getItem('token');
+
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	return axios
+		.get(`/api/posts/${postId}/comments`, config)
+		.then((res) => res.data);
+};
+
+export const createComment = (postId, text) => {
+	const token = localStorage.getItem('token');
+
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`,
+		},
+	};
+
+	return axios
+		.post(`/api/posts/${postId}`, { text }, config)
+		.then((res) => res.data);
+};
