b592d9ac8edc1038fefa0107d3bbcb47df547dd5
BRITHE <bthellier51@gmail.com>
2023-03-31 10:41:23 +0200

    added some backend to get ready for deployment ?

--- server/src/index.js
+++ server/src/index.js
@@ -9,17 +9,14 @@ import { protect } from './middleware/auth.js';
 import { login, register } from './handlers/user.js';
 import router from './router/router.js';
 
+import { notFound, errorHandler } from '../src/middleware/error.js';
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-	res.status(200);
-	res.json({ message: 'watzeid - API v1' });
-});
 
 app.use('/api', protect, router);
 app.post('/register', register);
@@ -28,9 +25,17 @@ app.post('/login', login);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.listen(process.env.PORT, () => {
-	console.log(`server on http://localhost:${process.env.PORT}`);
-});
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('watzeid - API v1');
+	});
+}
 
 // app.use((err, req, res, next) => {
 // 	if (err.type === 'auth') {
@@ -43,5 +48,16 @@ app.listen(process.env.PORT, () => {
 // 			.json({ message: 'Our fault, the server is probably burning ! :/' });
 // 	}
 // });
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}.`)
+);
 
 export default app;
